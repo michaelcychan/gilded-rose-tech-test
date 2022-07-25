3826e94447713bde44c24bf05cce268d2a13a78c
Michael Chan <michaelcychan@gmail.com>
2022-07-25 21:41:11 +0100

    added eslint and jest test into one command

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,16 +14,16 @@ class Shop {
   }
   updateQuality() { // needs substantial refactoring
     for (let i = 0; i < this.items.length; i++) { // can it be changed to forEach or map?
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality -= 1;
           }
         }
       } else { // name == 'Aged Brie' or name == 'Backstage passes to a TAFKAL80ETC concert'
         if (this.items[i].quality < 50) {
           this.items[i].quality += 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality += 1;
@@ -37,14 +37,14 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn -=  1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != "Aged Brie") {
+          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality -= 1;
               }
             }
