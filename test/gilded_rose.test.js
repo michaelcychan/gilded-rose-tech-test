926d6f01ff8b5565b2d2db122e1f12107e60c0e7
Michael Chan <michaelcychan@gmail.com>
2022-07-26 11:32:50 +0100

    added tests for resutls after 10 days

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -56,4 +56,78 @@ describe("Gilded Rose", function() {
       expect(items[0].quality).toBe(50);
     });
   });
+  describe("Existing Items after 10 day", () => {
+    it("calculates Aged Brie correctly", () => {
+      const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-8);
+      expect(items[0].quality).toBe(18);
+    });
+    it("calculates +5 Dexterity Vest correctly", () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(10);
+    });
+    it("calculates Elixir of the Mongoose correctly", () => {
+      const gildedRose = new Shop([new Item("Elixir of the Mongoose", 5, 7)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-5);
+      expect(items[0].quality).toBe(0);
+    });
+    it("calculates Sulfuras, Hand of Ragnaros (case 1) correctly", () => {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(80);
+    });
+    it("calculates Sulfuras, Hand of Ragnaros (case 2) correctly", () => {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(80);
+    });
+    it("calculates Backstage passes to a TAFKAL80ETC concert (case 1) correctly", () => {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(5);
+      expect(items[0].quality).toBe(35);
+    });
+    it("calculates Backstage passes to a TAFKAL80ETC concert (case 2) correctly", () => {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(50);
+    });
+    it("calculates Backstage passes to a TAFKAL80ETC concert (case 3) correctly", () => {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+      for (let i = 1; i < 10; i++ ) {
+        gildedRose.updateQuality();
+      }
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-5);
+      expect(items[0].quality).toBe(0);
+    });
+  });
 });
