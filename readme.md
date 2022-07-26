86f4afae2616d19b29f43cf3fc2a83c384d78a42
Michael Chan <michaelcychan@gmail.com>
2022-07-26 09:56:56 +0100

    added tests for existing items

--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@ This is an Individual Technical Challenge at Week 10 in Makers Academy Bootcamp.
 Link to all Individual technical challenges: https://github.com/makersacademy/course/tree/main/individual_challenges
 
 
+## Original Program:
+According to the source, Conjured Mana Cake is a new addition to the Inn, therefore the computer program could not handle its SellIn and Quality correctly.
+
+
+![Screenshot of Original Program](./screenshots/OriginalOutput-Screenshot.png)
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -6,4 +6,54 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+  describe("Existing Items after 1 day", () => {
+    it("calculates Aged Brie correctly", () => {
+      const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(1);
+      expect(items[0].quality).toBe(1);
+    });
+    it("calculates +5 Dexterity Vest correctly", () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(19);
+    });
+    it("calculates Elixir of the Mongoose correctly", () => {
+      const gildedRose = new Shop([new Item("Elixir of the Mongoose", 5, 7)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(4);
+      expect(items[0].quality).toBe(6);
+    });
+    it("calculates Sulfuras, Hand of Ragnaros (case 1) correctly", () => {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(80);
+    });
+    it("calculates Sulfuras, Hand of Ragnaros (case 2) correctly", () => {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(80);
+    });
+    it("calculates Backstage passes to a TAFKAL80ETC concert (case 1) correctly", () => {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(14);
+      expect(items[0].quality).toBe(21);
+    });
+    it("calculates Backstage passes to a TAFKAL80ETC concert (case 2) correctly", () => {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(50);
+    });
+    it("calculates Backstage passes to a TAFKAL80ETC concert (case 3) correctly", () => {
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(4);
+      expect(items[0].quality).toBe(50);
+    });
+  });
 });
